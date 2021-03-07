f2585e07bbf3e9a01e08a08d8f9efc9e4a42bd50
yuequ <yueh33@qq.com>
2021-03-07 20:52:53 +0800

    test: script 1.3

--- workflows/script.js
+++ workflows/script.js
@@ -3,7 +3,7 @@ module.exports = async function ({ helpers }) {
     "https://app.asana.com/api/1.0/tags/1198197485416173/tasks?completed_since=now&opt_fields=name,due_at,modified_at",
     {headers: {'Authorization': '${{ secrets.ASANA_TOKEN }}', 'content-type': 'application/json'}}
   );
-  const final = result.data;
+  var final = result.data;
   final["uuid"] = String(final.gid) + ":" + final.modified_at;
   return final;
 };
