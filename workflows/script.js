98c48ff13a0d7a3a2759f97add526d63e67f6d5e
yuequ <yueh33@qq.com>
2021-03-07 22:26:01 +0800

    test: script 1.11

--- workflows/script.js
+++ workflows/script.js
@@ -1,9 +1,11 @@
 module.exports = async function ({ helpers }) {
   const result = await helpers.axios.get(
     "https://app.asana.com/api/1.0/tags/1198197485416173/tasks?completed_since=now&opt_fields=name,due_at,modified_at",
-    {headers: {'Authorization': '${{ secrets.ASANA_TOKEN }}', 'content-type': 'application/json'}}
+    {
+        headers: {
+          Authorization: "Bearer <<<${{ secrets.ASANA_TOKEN_BEARER }}>>>"
+        }
+    }
   );
-  var final = result.data;
-  final["uuid"] = String(final.gid) + ":" + final.modified_at;
-  return final;
+  return result.data;
 };
